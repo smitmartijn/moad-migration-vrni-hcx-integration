7d005d562f0d465adb5062f9d55ac71995adeff6
Martijn Smit <martijn@lostdomain.org>
2020-03-09 13:57:41 +0100

    Require PowerShell v6+ Test if the right versions of VMware.VimAutomation.Hcx and PowervRNI are installed. Exit gracefully with context, if they are not. More code comments

--- sync-vrni-to-hcx.ps1
+++ sync-vrni-to-hcx.ps1
@@ -1,3 +1,5 @@
+#Requires -Version 6
+
 [CmdletBinding(DefaultParameterSetName="vRNI")]
 param (
     [Parameter(Mandatory=$true, ParameterSetName="vRNI")]
@@ -40,9 +42,27 @@ param (
         [string]$vRNI_Cloud_API_Token
 )
 
-Import-Module VMware.VimAutomation.Hcx -Force
-Import-Module "./modules/VMware.HCX.MobilityGroups.psm1" -Force
-Import-Module "./modules/PowervRNI/PowervRNI.psm1" -Force
+# Load required modules; PowerCLI HCX, a custom extension to that module, and PowervRNI
+$moduleHCX = Get-InstalledModule -Name VMware.VimAutomation.Hcx
+if(!($moduleHCX)) {
+    throw "Required module 'VMware.VimAutomation.Hcx' is missing. Please install it by using: Install-Module VMware.VimAutomation.Hcx"
+}
+if ([version]$moduleHCX.Version -lt [version]"11.5") {
+    throw "Required module 'VMware.VimAutomation.Hcx' needs to be updated to 11.5+. It's at '$($moduleHCX.Version)' now. Please upgrade it by using: Update-Module VMware.VimAutomation.Hcx"
+}
+Import-Module VMware.VimAutomation.Hcx
+# Load the HCX extension for Mobility Group management
+Import-Module "./modules/VMware.HCX.MobilityGroups.psm1"
+
+# PowervRNI
+$modulePowervRNI = Get-InstalledModule -Name PowervRNI
+if(!($modulePowervRNI)) {
+    throw "Required module 'PowervRNI' is missing. Please install it by using: Install-Module PowervRNI"
+}
+if ([version]$modulePowervRNI.Version -lt [version]"1.8") {
+    throw "Required module 'PowervRNI' needs to be updated to 1.8+. It's at '$($modulePowervRNI.Version)' now. Please upgrade it by using: Update-Module PowervRNI"
+}
+Import-Module PowervRNI
 
 # @lamw function
 Function My-Logger {
@@ -59,6 +79,7 @@ Function My-Logger {
     Write-Host -ForegroundColor $color " $message"
 }
 
+# Are we connecting to vRNI on-prem or vRNI Cloud?
 if($PSCmdlet.ParameterSetName -eq "vRNI")
 {
     My-Logger -message "Connecting to vRealize Network Insight.."
@@ -76,12 +97,18 @@ if(!$connectionvRNI) {
 # First, get a list of all applications that are in vRNI
 My-Logger -message "Retrieving applications from vRNI.."
 # Are we looking for specified applications?
-if($Sync_Applications.Count -gt 0) {
+if($Sync_Applications.Count -gt 0)
+{
+    # If we are looking for specified applications, go through the specified apps and look them up individually
     $vRNI_applications = @()
     foreach($app in $Sync_Applications) {
         $vRNI_app = Get-vRNIApplication -Connection $connectionvRNI -Name $app
         if($vRNI_app) {
+            # Save the located app to the array that we'll pass onto HCX
             $vRNI_applications += $vRNI_app
+        }
+        else {
+            My-Logger -message "Specified application '$($app)' not found - skipping" -color "Yellow"
         }
 
     }
@@ -91,22 +118,28 @@ else {
     $vRNI_applications = Get-vRNIApplication -Connection $connectionvRNI
 }
 
+# Now that we've got the applications, let's find the VMs associated with this application
 $application_list = @()
 
 foreach($app in $vRNI_applications)
 {
+    # Placeholder record to add to the array that is being passed to HCX later
     $application_record = @{
         "name" = $app.name
         "members" = @()
     }
+    # Retrieve the member VMs on the current application
     $memberVMs = Get-vRNIApplicationMemberVM -Application $app
     $entityIDs = @()
-    foreach($memberVM in $memberVMs) {
+    foreach($memberVM in $memberVMs)
+    {
+        # Only save VirtualMachines for now
         if($memberVM.entity_type -eq "VirtualMachine") {
             $entityIDs += $memberVM.entity_id
         }
     }
 
+    # Continue to the next application, if we don't have any VMs
     if(!($entityIDs)) {
         continue
     }
